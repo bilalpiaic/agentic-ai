920e17d792bcea9c535ac1ff356e2bb0b50ac151
Muhammad Zain Attiq <zainatteeq@gmail.com>
2025-01-08 00:07:20 +0500

    different memories added

--- langchain/langchain_memory.py
+++ langchain/langchain_memory.py
@@ -8,9 +8,13 @@ load_dotenv()
 llm = ChatGoogleGenerativeAI(
     model="gemini-1.5-flash", google_api_key=os.getenv("GOOGLE_API_KEY"))
 
-memory = ConversationSummaryBufferMemory(llm=llm, max_token_limit=1000)
+memory1 = ConversationBufferMemory()
+memory2 = ConversationBufferWindowMemory(k = 2)
+memory3 = ConversationSummaryMemory(llm=llm)
+memory4 = ConversationSummaryBufferMemory(llm=llm, max_token_limit=1000)
 
-chain = ConversationChain(llm=llm, memory=memory)
+# Put here the memory which one you want to use
+chain = ConversationChain(llm=llm, memory=memory1)
 
 while True: 
     user_input = input("You: ")
