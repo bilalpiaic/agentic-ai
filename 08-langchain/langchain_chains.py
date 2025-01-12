2fc76c663663513901961c1936078e5a110ee3c1
devnaveedsarwar <learnwithnaveedsarwar@gmail.com>
2025-01-12 16:31:43 +0500

    Lanchain chains

--- 08-langchain/langchain_chains.py
+++ 08-langchain/langchain_chains.py
@@ -1,5 +1,6 @@
 
 from langchain_openai import ChatOpenAI
+from langchain_google_genai import GoogleGenerativeAI
 from langchain_huggingface import HuggingFaceEndpoint
 from langchain_core.prompts import PromptTemplate
 from langchain.chains import SimpleSequentialChain, LLMChain
@@ -8,40 +9,39 @@ from dotenv import load_dotenv
 import os
 load_dotenv()
 
-llm = ChatOpenAI(openai_api_key=os.getenv("OPENAI_API"))
 
-llm2 = HuggingFaceEndpoint(
-    repo_id="HuggingFaceH4/zephyr-7b-beta",
-    huggingfacehub_api_token= os.getenv("HUGGINGFACE_TOKEN")
+# llm_for_keyword = HuggingFaceEndpoint(
+#     repo_id="HuggingFaceH4/zephyr-7b-beta",
+#     huggingfacehub_api_token= os.getenv("HUGGINGFACE_TOKEN")
+# )
+
+llm = GoogleGenerativeAI(
+     model="gemini-1.5-flash",
+     google_api_key=os.getenv("GOOGLE_API_KEY")
 )
 
-
-
-prompt0 = PromptTemplate(template="Context: of this chat", inputVriable=["chat"])
-
-
-
-prompt1 = PromptTemplate(template="Give me the famous place of {Text}", inputVriable=["text"])
-
-prompt2 = PromptTemplate(template="Wha is the location of place {text}", inputVriable=["text"])
+# llm_for_description = ChatOpenAI(openai_api_key=os.getenv("OPENAI_API"))
 
 
 
-chain1 = LLMChain(llm=llm, prompt=prompt1)
-chain2 = LLMChain(llm=llm, prompt=prompt2)
 
-chain = SimpleSequentialChain(chains=[chain1, chain2])
+prompt_for_keywords = PromptTemplate(template="Define keyword for article writing, user give the article title is: {input}", input_variables=["input"])
 
-result = chain.invoke("Faisalabad")
+prompt_for_outline = PromptTemplate(template="Write outline for an article using keywords {input}", input_variables=["input"])
+
+prompt_for_description = PromptTemplate(template="Write the detailed article following the provided outline is: {input}", input_variables=["input"])
+
+
+
+keywords_step = LLMChain(llm=llm, prompt=prompt_for_keywords)
+outline_step = LLMChain(llm=llm, prompt=prompt_for_outline)
+description_step = LLMChain(llm=llm, prompt=prompt_for_description)
+
+chain = SimpleSequentialChain(chains=[keywords_step, outline_step, description_step])
+
+result = chain.invoke({"input": "what is the best way to learn programming?"})
 
 print(result)
 
 
 
-
-# from langchain_huggingface import HuggingFaceEndpoint
-
-# llm = HuggingFaceEndpoint(
-#     repo_id="HuggingFaceH4/zephyr-7b-beta"
-#     huggingfacehub_api_token= "sdf"
-# )
