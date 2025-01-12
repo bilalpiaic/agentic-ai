f5b3416fbfe96c1309da6db96370843134f90e67
devnaveedsarwar <learnwithnaveedsarwar@gmail.com>
2025-01-12 16:44:48 +0500

    llm streaming

--- 07-llm-and-prompt-engineering/02_gemeni_llm_stream.py
+++ 07-llm-and-prompt-engineering/02_gemeni_llm_stream.py
@@ -1,9 +1,15 @@
 from langchain_google_genai import GoogleGenerativeAI
 from langchain_core.prompts import PromptTemplate
+from dotenv import load_dotenv
+import os
 import sys
+load_dotenv()
 
 
-llm = GoogleGenerativeAI(model="gemini-1.5-flash", google_api_key="AIzaSyDIrIP-uFh7gZzjV_PUvHwOG6SE1xzpLuc")
+llm = GoogleGenerativeAI(
+    model="gemini-1.5-flash",
+    google_api_key=os.getenv("GOOGLE_API_KEY")
+    )
 
 template = """Question: {question}
 
