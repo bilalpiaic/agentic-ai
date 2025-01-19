d4b0c9dee9dd6dde9b603bb3dc50242b1a3ba576
devnaveedsarwar <learnwithnaveedsarwar@gmail.com>
2025-01-19 15:55:10 +0500

    Rag example file path change

--- 08-langchain/07_gemeni_rag.py
+++ 08-langchain/07_gemeni_rag.py
@@ -11,10 +11,14 @@ llm = GoogleGenerativeAI(model="gemini-1.5-flash", google_api_key=os.getenv("GOO
 
 memory = ConversationBufferWindowMemory(k=5)
 
+loader = None
 try:
-    loader = TextLoader("data.txt")
-except Exception as e:  
+    loader = TextLoader("./data/data.txt")
+except Exception as e:
     print("Error while loading file=", e)
+
+if loader is None:
+    raise ValueError("Failed to load the file.")
 
 # Create embeddings
 embedding = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
@@ -27,80 +31,10 @@ index_creator = VectorstoreIndexCreator(
     embedding=embedding,
     text_splitter=text_splitter
 )
-index = index_creator.from_loaders([loader])
+store = index_creator.from_loaders([loader])
 
 # Query the index with the LLM
 while True:
-    human_message = input("How i can help you today? ")
-    response = index.query(human_message, llm=llm, memory=memory)
+    human_message = input("How can I help you today? ")
+    response = store.query(human_message, llm=llm, memory=memory)
     print(response)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# from langchain_community.document_loaders import TextLoader
-# from langchain.indexes import VectorstoreIndexCreator
-# from langchain.text_splitter import CharacterTextSplitter
-# from langchain_google_genai import GoogleGenerativeAI, GoogleGenerativeAIEmbeddings
-# import os
-# from dotenv import load_dotenv
-# load_dotenv()
-
-# llm = GoogleGenerativeAI(model="gemini-1.5-flash", google_api_key=os.getenv("GOOGLE_API_KEY"))
-
-# try:
-#     loader = TextLoader("data.txt")
-# except Exception as e:
-#     print("Error while loading file=", e)
-
-# # Create embeddings
-# embedding = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
-
-# # Use a smaller chunk size to manage token limits
-# text_splitter = CharacterTextSplitter(chunk_size=500, chunk_overlap=100)
-
-# # Create the index with the specified embedding model and text splitter
-# index_creator = VectorstoreIndexCreator(
-#     embedding=embedding,
-#     text_splitter=text_splitter
-# )
-# index = index_creator.from_loaders([loader])
- 
-# # Query the index with the LLM
-# response = index.query("What is the name of your hotel?", llm=llm)
-# print(response)
