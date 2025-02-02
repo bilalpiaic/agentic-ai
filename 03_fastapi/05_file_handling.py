7d93ba6dda3407ab5d882fabeb18bba443740bf8
devnaveedsarwar <learnwithnaveedsarwar@gmail.com>
2025-02-02 18:51:37 +0500

    fix validations

--- 03_fastapi/05_file_handling.py
+++ 03_fastapi/05_file_handling.py
@@ -1,21 +1,21 @@
 # poetry add python-multipart
-from fastapi import FastAPI,File, UploadFile, Form
+from fastapi import FastAPI,File, UploadFile
 from fastapi.staticfiles import StaticFiles
 
 import shutil
 import os
 
 UPLOAD_FOLDER = "03_fastapi/uploads"  # Define the folder to store uploaded files
+
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 app = FastAPI()
-
 
 # Mount the uploads folder to serve static files
 app.mount("/static", StaticFiles(directory=UPLOAD_FOLDER), name="static")
 
 @app.post("/upload/")
-async def upload_file(file: UploadFile = File(...), description: str = Form(...)):
+async def upload_file(file: UploadFile = File(...)):
     file_path = os.path.join(UPLOAD_FOLDER, file.filename)
 
     with open(file_path, "wb") as buffer:
@@ -24,4 +24,5 @@ async def upload_file(file: UploadFile = File(...), description: str = Form(...)
     # Return the public URL to access the file
     file_url = f"/static/{file.filename}"
 
-    return {"filename": file.filename, "description": description, "file_url": file_url}
+    return {"filename": file.filename, "file_url": file_url}
+
