e1050a78e925cb177198bc36ef3e69612614665f
Muhammad Zain Attiq <zainatteeq@gmail.com>
2025-02-23 10:20:42 +0500

    Added CrewAI flows project examples and detailed concepts

---
+++ 10_crewai/and_flow.py
@@ -0,0 +1,19 @@
+from crewai.flow.flow import Flow, and_, listen, start
+
+class AndExampleFlow(Flow):
+
+    @start()
+    def start_method(self):
+        self.state["greeting"] = "Hello from the start method"
+
+    @listen(start_method)
+    def second_method(self):
+        self.state["joke"] = "What do computers eat? Microchips."
+
+    @listen(and_(start_method, second_method))
+    def logger(self):
+        print("---- Logger ----")
+        print(self.state)
+
+flow = AndExampleFlow()
+flow.kickoff()
---
+++ 10_crewai/or_flow.py
@@ -0,0 +1,21 @@
+from crewai.flow.flow import Flow, listen, or_, start
+
+class OrExampleFlow(Flow):
+
+    @start()
+    def start_method(self):
+        return "Hello from the start method"
+
+    @listen(start_method)
+    def second_method(self):
+        return "Hello from the second method"
+
+    @listen(or_(start_method, second_method))
+    def logger(self, result):
+        print(f"Logger: {result}")
+
+
+
+flow = OrExampleFlow()
+flow.kickoff()
+flow.plot()
