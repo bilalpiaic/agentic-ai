5d2d7f51cd008ffa1c91674aa5ee07a516aab81c
devnaveedsarwar <learnwithnaveedsarwar@gmail.com>
2025-03-02 06:51:27 +0500

    auth guide

--- 04_databases/02_fast_api_postgress/main.py
+++ 04_databases/02_fast_api_postgress/main.py
@@ -1,16 +1,19 @@
 from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 from config.database import SessionLocal, engine
-from models.todo_model import Todo
-from pydantic import BaseModel
+from models.todo_model import Todos, Users
+from pydantic import BaseModel,Field,AfterValidator
+from typing_extensions import Annotated
 from typing import List
 
 # Create database tables
-Todo.metadata.create_all(bind=engine)
+Todos.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
 # Dependency for DB session
+
+
 def get_db():
     db = SessionLocal()
     try:
@@ -19,42 +22,103 @@ def get_db():
         db.close()
 
 # Pydantic model for validation
+
+
 class TodoCreate(BaseModel):
     title: str
-    description: str = None # type: ignore
+    description: str = None  # type: ignore
     completed: bool = False
+
+
+def is_even(value: int) -> int:
+    if value % 2 == 1:
+        raise ValueError(f'{value} is not an even number')
+    return value
+class UserCreate(BaseModel):
+    name: Annotated[str, Field(min_length=3,max_length=50)]
+    email: Annotated[str, Field(pattern=r'^\S+@\S+$')]
+    password: Annotated[str, Field(min_length=6)]
+    number: Annotated[int, AfterValidator(is_even)]
+
 
 class TodoResponse(TodoCreate):
     id: int
+
     class Config:
         orm_mode = True
 
 # Create a new Todo
-@app.post("/todos/", response_model=TodoResponse)
-def create_todo(todo: TodoCreate, db: Session = Depends(get_db)):
-    db_todo = Todo(title=todo.title, description=todo.description, completed=todo.completed)
-    db.add(db_todo)
-    db.commit()
-    db.refresh(db_todo)
-    return db_todo
+
+
+@app.post("/todos/{user_id}")
+def create_todo(user_id, todo: TodoCreate, db: Session = Depends(get_db)):
+    try:
+        db_todo = Todos(title=todo.title, description=todo.description,
+                        completed=todo.completed, user_id=user_id)
+        db.add(db_todo)
+        db.commit()
+        db.refresh(db_todo)
+        return {
+            "data": db_todo,
+            "message": "Todo created successfully",
+            "status": "success"
+        }
+    except Exception as e:
+        print('An exception occurred')
+        print(e)
+        return {
+            "message": str(e),
+            "status": "error",
+            "data": None
+        }
+
+
+@app.post("/create_user/")
+def create_user(user: UserCreate, db: Session = Depends(get_db)):
+    try:
+        valid_user = Users(name=user.name, email=user.email,
+                           password=user.password)
+        db.add(valid_user)
+        db.commit()
+        db.refresh(valid_user)
+        return {
+            "data": valid_user,
+            "message": "Todo created successfully",
+            "status": "success"
+        }
+    except Exception as e:
+        print('An exception occurred')
+        print(e)
+        return {
+            "message": str(e),
+            "status": "error",
+            "data": None
+        }
+
 
 # Get all Todos
-@app.get("/todos/")
-def get_todos(db: Session = Depends(get_db)):
-    return db.query(Todo).all()
+
+
+@app.get("/todos/{user_id}", response_model=List[TodoResponse])
+def get_todos(user_id:str,db: Session = Depends(get_db)):
+    return db.query(Todos).all()
 
 # Get a Todo by ID
+
+
 @app.get("/todos/{todo_id}", response_model=TodoResponse)
 def get_todo(todo_id: int, db: Session = Depends(get_db)):
-    todo = db.query(Todo).filter(Todo.id == todo_id).first()
+    todo = db.query(Todos).filter(Todos.id == todo_id).first()
     if not todo:
         raise HTTPException(status_code=404, detail="Todo not found")
     return todo
 
 # Update a Todo
+
+
 @app.put("/todos/{todo_id}", response_model=TodoResponse)
 def update_todo(todo_id: int, todo_update: TodoCreate, db: Session = Depends(get_db)):
-    todo = db.query(Todo).filter(Todo.id == todo_id).first()
+    todo = db.query(Todos).filter(Todos.id == todo_id).first()
     if not todo:
         raise HTTPException(status_code=404, detail="Todo not found")
 
@@ -66,12 +130,14 @@ def update_todo(todo_id: int, todo_update: TodoCreate, db: Session = Depends(get
     return todo
 
 # Delete a Todo
+
+
 @app.delete("/todos/{todo_id}", response_model=dict)
 def delete_todo(todo_id: int, db: Session = Depends(get_db)):
-    todo = db.query(Todo).filter(Todo.id == todo_id).first()
+    todo = db.query(Todos).filter(Todos.id == todo_id).first()
     if not todo:
         raise HTTPException(status_code=404, detail="Todo not found")
-    
+
     db.delete(todo)
     db.commit()
     return {"message": "Todo deleted"}
@@ -79,4 +145,4 @@ def delete_todo(todo_id: int, db: Session = Depends(get_db)):
 
 # uv run alembic revision --autogenerate -m "create todos table"
 
-# alembic upgrade head
+# uv run alembic upgrade head
--- 04_databases/02_fast_api_postgress/models/todo_model.py
+++ 04_databases/02_fast_api_postgress/models/todo_model.py
@@ -1,12 +1,45 @@
-from sqlalchemy import Column, Integer, String, Boolean
+from tabnanny import check
+from sqlalchemy import Column, Integer, String, Boolean,ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
+
+
+from sqlalchemy import Column, Integer, String, Boolean,CheckConstraint
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
-class Todo(Base):
+
+# class Address(Base):
+#     __tablename__ = "address"
+    
+#     id = Column(Integer, primary_key=True, index=True)
+#     zip_code = Column(String, index=True)
+#     street = Column(String, nullable=True)
+#     city = Column(String, nullable=True)
+#     user_id = Column(Integer, ForeignKey('users.id', ondelete='CASCADE'))
+
+    
+class Users(Base):
+    __tablename__ = "users"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, CheckConstraint('value > 5'),  index=True)
+    email = Column(String, CheckConstraint('email ~* "^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"'), nullable=False, unique=True, ) # type: ignore
+    password = Column(String, nullable=True)
+
+
+
+
+class Todos(Base):
     __tablename__ = "todos"
     
     id = Column(Integer, primary_key=True, index=True)
-    title = Column(String, index=True)
+    title = Column(String, index=True, nullable=False)
     description = Column(String, nullable=True)
     completed = Column(Boolean, default=False)
+    user_id = Column(Integer, ForeignKey('users.id', ondelete='CASCADE'), nullable=False)
+    
+    user = relationship("Users", back_populates="todos")
